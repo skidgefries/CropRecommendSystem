e0c9b2cfa3ecdb6f443a96a9f23703d43f16bbc5
1014Aayush <aayushpokhre@gmail.com>
2024-05-14 00:23:00 +0545

    Dashboard Start

--- Frontend/Crop/src/Pages/Dashboard.jsx
+++ Frontend/Crop/src/Pages/Dashboard.jsx
@@ -6,7 +6,19 @@ export default function Dashboard()
     return(
         
         <>
+        <div className="PlantVitals">
+            <div className="TopV">
+                <div className="TopLeft">
+                    <h1>Plant Vitals</h1>
+                    <div className="Moisture"></div>
+                </div>
+                <div className="TreeImg"></div>
+            </div>
+            <div className="MiddleV"></div>
+            <div className="BottomV"></div>
 
+
+        </div>
 
         {/* code here */}
 
